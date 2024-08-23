db83abc0685f7f3d543eee308fd3e14ff6188f71
GREEN <GREEN@G704-00>
2024-08-23 10:45:25 +0900

    project setting completed.

---
+++ src/main/java/com/study/project/TeamProject17thApplication.java
@@ -0,0 +1,13 @@
+package com.study.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject17thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject17thApplication.class, args);
+	}
+
+}
